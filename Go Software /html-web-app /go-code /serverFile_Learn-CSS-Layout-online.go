fd3a98a88ce72368384aa7b17af9198c62a5d6ba
kyle2501@gmail.com <kyle2501@gmail.com>
2024-07-08 08:03:12 -0700

    update serverFile_Learn-CSS-Layout-online.go

--- Go Software /html-web-app /go-code /serverFile_Learn-CSS-Layout-online.go
+++ Go Software /html-web-app /go-code /serverFile_Learn-CSS-Layout-online.go
@@ -10,6 +10,20 @@ import (
 )
 
 
+type htmlPageData struct {
+    pageTitle string
+    pagePath string
+    pageList []pageNav
+    
+}
+
+
+type pageNav struct {
+    pageTitle string
+    pageLink string
+}
+
+
 func app_welcome_center_page() {
 
 
